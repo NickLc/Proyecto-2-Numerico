0b3a1f8774eb05656318d24f0822b6b954f5bdc8
Vpp2000 <48797063+Vpp2000@users.noreply.github.com>
2019-05-26 14:18:35 -0500

    Modificando Lagrange.

--- CODIGO/interpolagrange.py
+++ CODIGO/interpolagrange.py
@@ -1,11 +1,48 @@
+# Ln_i: Retorna el coeficiente de Lagrange L_ni
+def Ln_i(n, i, x_vector, x):
+	p1 = 1
+	p2 = 1
 
-# t es el valor que queremos evaluar en el polinomio
-def interpol_Lagrange(x0, x1, x2, y0, y1, y2, t):
-    p = y0*(t-x1)*(t-x2)/((x0-x1)*(x0-x2)) + y1*(t-x0)*(t-x2)/((x1-x0)*(x1-x2)) + y2*(t-x1)*(t-x0)/((x2-x1)*(x2-x0)) 
-    
-    print(p)
-    
-    
-interpol_Lagrange(1, 4, 6, 1.5709, 1.5727, 1.5751, 3.5)    
-#f(xi)=yi
-#quiero aproximar t
+	for j in range(0, n):
+		if i != j:
+			p1 *= (x - x_vector[j])
+			p2 *= (x_vector[i] - x_vector[j])
+	
+	return p1/p2
+
+# INT_Lagrange: Retorna el polinomio generado P_n_eq, listo para evaluar cualquier
+# x y tomar el valor de la curva interpolada en cualquier punto
+def INT_Lagrange(x_vector, y_vector, verbose=False):
+	#=========================================
+	if verbose:
+		print("=========================================")
+		print("METODO DE LAGRANGE")
+		print("Este metodo genera una funcion P_n(x)\nrepresentando al polinomio interpolado por Lagrange")
+		print("Se usara los datos de \'x\':", x_vector)
+		print("Se usara los datos de \'y\':", y_vector)
+		print("=========================================")
+	#=========================================
+
+	def P_n_eq(x, verbose=False):
+		#=========================================
+		if verbose:
+			print("-----------------------------------")
+			print("Evaluando x = {}".format(x))
+		#=========================================
+		n = x_vector.size
+		acc = 0
+		for i in range(y_vector.size):
+			#=========================================
+			if verbose:
+				print("L_{},{}:{}".format(n, i, Ln_i(n, i, x_vector, x)))
+			#=========================================
+			acc += y_vector[i]*Ln_i(n, i, x_vector, x)
+
+		#=========================================
+		if verbose:
+			print("Finalmente, P_{}({}):{}".format(n, x, acc))
+			print("-----------------------------------")
+		#=========================================
+		return acc
+	return P_n_eq
+
